623a93efeda7c4854a130ab2b510b63ed3785158
Ryuichiro Kyotani <rira.18ki@gmail.com>
2024-07-06 00:19:32 +0900

    ⚡️ メイン・ベース・アクセントカラーの決定 ✨ Headerの作成

---
+++ app/src/components/header/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Header.css';
+
+export default function Header() {
+  return (
+    <header className='header'>
+        <button>About</button>
+        <button>Skills</button>
+        <button>Works</button>
+        <button>Connects</button>
+    </header>
+  )
+}
